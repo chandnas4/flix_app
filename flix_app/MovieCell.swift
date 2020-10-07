4fc12bdafc40473cb1e8e2878353454f1aba5124
Shubham Chandna <shubhamchandna2002@gmail.com>
2020-10-07 13:53:30 +0530

    Part 2 of the flix app

---
+++ flix_app/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flix_app
+//
+//  Created by Shubham Chandna on 9/29/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flix_app/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix_app
+//
+//  Created by Shubham Chandna on 10/7/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
